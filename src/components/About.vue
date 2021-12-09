d1690a3f0bb7200a02b4e1b71d5189c80ce1b2fd
anil kumar thakur <anilkumarthakur60@gmail.com>
2021-12-09 22:19:50 +0545

    commit

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,49 +1,73 @@
 <template>
-<v-row>
-    <v-col lg="6" md="6" sm="12" >
-            <v-img src="@/assets/anil.jpg"   > </v-img>
+  <v-row>
+    <v-col lg="6" md="6" sm="12">
+      <v-img src="@/assets/anil.jpg"> </v-img>
     </v-col>
     <v-col lg="6" md="6" sm="12">
-        <h3 class=" font-weight-bold text-sm-h3 text-lg-h3 text-md-h3 my-5">About Me</h3>
-        <h5 class=" font-weight-bold text-sm-h5 text-lg-h5 text-md-h5 my-5">Creative Software Developer based in Kathmandu, Nepal</h5>
-        
-        <p class="text-justify font-weight-medium ">
-           I'm  Web Developer, and very passionate and dedicated to my work. With 1year plus years experience as a professional web developer, I have acquired the skills and knowledge necessary to make your project a success. I enjoy every step of the design process, from discussion and collaboration.
-            <br>
-            <br>
-            I design and develop services for customers of all sizes, specializing in creating stylish, modern websites, web services and online stores. My passion is to design digital user experiences through the bold interface and meaningful interactions. Check out my Portfolio
-            </p>
+      <h3 class="font-weight-bold text-sm-h3 text-lg-h3 text-md-h3 my-5">
+        About Me
+      </h3>
+      <h5 class="font-weight-bold text-sm-h5 text-lg-h5 text-md-h5 my-5">
+        Creative Software Developer based in Kathmandu, Nepal
+      </h5>
 
-            <div class="d-flex">
-                
-                <v-icon color="success " class="mr-4" >mdi-check-all</v-icon>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolorem alias, nostrum ipsum repellat, excepturi numquam nulla aut officia aspernatur pariatur consequatur a tempore dolorum obcaecati error expedita totam tenetur?</p>
-            </div>
-            <div class="d-flex">
-                
-                <v-icon color="success " class="mr-4" >mdi-check-all</v-icon>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolorem alias, nostrum ipsum repellat, excepturi numquam nulla aut officia aspernatur pariatur consequatur a tempore dolorum obcaecati error expedita totam tenetur?</p>
-            </div>
-            <div class="d-flex">
-                
-                <v-icon color="success " class="mr-4" >mdi-check-all</v-icon>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid dolorem alias, nostrum ipsum repellat, excepturi numquam nulla aut officia aspernatur pariatur consequatur a tempore dolorum obcaecati error expedita totam tenetur?</p>
-            </div>
- 
-       
-            <v-btn color="success" href="https://drive.google.com/file/d/1IcK62u-Mvrwk_RMawI0d_XZ8GlkBALUn/view?usp=sharing" target="_blank">
-                Download My Resume
-            </v-btn>
+      <p class="text-justify font-weight-medium">
+        I'm Web Developer, and very passionate and dedicated to my work. With
+        1year plus years experience as a professional web developer, I have
+        acquired the skills and knowledge necessary to make your project a
+        success. I enjoy every step of the design process, from discussion and
+        collaboration.
+        <br />
+        <br />
+        I design and develop services for customers of all sizes, specializing
+        in creating stylish, modern websites, web services and online stores. My
+        passion is to design digital user experiences through the bold interface
+        and meaningful interactions. Check out my Portfolio
+      </p>
 
+      <div class="d-flex">
+        <v-icon color="success " class="mr-4">mdi-check-all</v-icon>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          dolorem alias, nostrum ipsum repellat, excepturi numquam nulla aut
+          officia aspernatur pariatur consequatur a tempore dolorum obcaecati
+          error expedita totam tenetur?
+        </p>
+      </div>
+      <div class="d-flex">
+        <v-icon color="success " class="mr-4">mdi-check-all</v-icon>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          dolorem alias, nostrum ipsum repellat, excepturi numquam nulla aut
+          officia aspernatur pariatur consequatur a tempore dolorum obcaecati
+          error expedita totam tenetur?
+        </p>
+      </div>
+      <div class="d-flex">
+        <v-icon color="success " class="mr-4">mdi-check-all</v-icon>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+          dolorem alias, nostrum ipsum repellat, excepturi numquam nulla aut
+          officia aspernatur pariatur consequatur a tempore dolorum obcaecati
+          error expedita totam tenetur?
+        </p>
+      </div>
+
+      <v-btn
+        color="success"
+        href="https://drive.google.com/file/d/1IcK62u-Mvrwk_RMawI0d_XZ8GlkBALUn/view?usp=sharing"
+        target="_blank"
+      >
+        Download My Resume
+      </v-btn>
     </v-col>
-</v-row>
+  </v-row>
 </template>
 <script>
 export default {
-    name:"About",
-      mounted() {  
-    document.title = "Anil Kumar Thakur | About Me";  
-  }, 
- 
-}
+  name: "About",
+  mounted() {
+    document.title = "Er.Anil Kumar Thakur| About Me";
+  },
+};
 </script>
--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,91 +1,73 @@
 <template>
-   <v-row>
-       <v-col sm="12" md="4" lg="4" v-for="img in imgs" :key="img">
-       
-          <v-responsive>
-           
-        <v-card class="mx-auto pa-4 " rounded="xl" 
-         
-        >
-           <v-img :src="img"  @click="showMultiple"    class="rounded-xl"></v-img>
+  <v-row>
+    <v-col sm="12" md="4" lg="4" v-for="img in imgs" :key="img">
+      <v-responsive>
+        <v-card class="mx-auto pa-4" rounded="xl">
+          <v-img :src="img" @click="showMultiple" class="rounded-xl"></v-img>
         </v-card>
-    
-          </v-responsive>
-     </v-col>
-    
+      </v-responsive>
+    </v-col>
 
-
-<v-col>
- 
-     <v-col sm="12" md="4" lg="3">
-      <vue-easy-lightbox
-                       
-            :visible="visible"
-            :imgs="imgs"
-            :index="index"
-            
-            @hide="handleHide"
-          >
-          
-          </vue-easy-lightbox>
-     </v-col>
-     
-</v-col>
-
-
-   </v-row>
-   
-
+    <v-col>
+      <v-col sm="12" md="4" lg="3">
+        <vue-easy-lightbox
+          :visible="visible"
+          :imgs="imgs"
+          :index="index"
+          @hide="handleHide"
+        >
+        </vue-easy-lightbox>
+      </v-col>
+    </v-col>
+  </v-row>
 </template>
 <script>
-import VueEasyLightbox from 'vue-easy-lightbox'
+import VueEasyLightbox from "vue-easy-lightbox";
 
 export default {
   components: {
-    VueEasyLightbox
+    VueEasyLightbox,
   },
   data() {
     return {
-            transparent: 'rgba(255, 255, 255, 0)',
+      transparent: "rgba(255, 255, 255, 0)",
 
-       imgs: [
-         require('../assets/portfolio/meriaamai.jpg'),
-            require('../assets/portfolio/videochat.jpg'),
-            require('../assets/portfolio/ecoomerce.jpg'),
-            require('../assets/portfolio/medmax.jpg'),
-            require('../assets/portfolio/medmaxchain.jpg'),
-            require('../assets/portfolio/blog.jpg'),
-            require('../assets/portfolio/visitnepal.jpg'),
-            require('../assets/portfolio/htmlportfolio.jpg'),
-            require('../assets/portfolio/calculator.jpg'),
-          
-         ],
+      imgs: [
+        require("../assets/portfolio/meriaamai.jpg"),
+        require("../assets/portfolio/videochat.jpg"),
+        require("../assets/portfolio/ecoomerce.jpg"),
+        require("../assets/portfolio/medmax.jpg"),
+        require("../assets/portfolio/medmaxchain.jpg"),
+        require("../assets/portfolio/blog.jpg"),
+        require("../assets/portfolio/visitnepal.jpg"),
+        require("../assets/portfolio/htmlportfolio.jpg"),
+        require("../assets/portfolio/calculator.jpg"),
+      ],
       visible: false,
-      index: 0   // default
-    }
+      index: 0, // default
+    };
   },
   methods: {
     showSingle() {
       this.imgs = {
-          title: 'this is a placeholder',
-          src: 'https://i.loli.net/2018/11/10/5be6852dec46e.jpeg'
-        }
-        this.show()
+        title: "this is a placeholder",
+        src: "https://i.loli.net/2018/11/10/5be6852dec46e.jpeg",
+      };
+      this.show();
     },
     showMultiple() {
-      this.index = 0  // index of imgList
-      this.show()
+      this.index = 0; // index of imgList
+      this.show();
     },
     show() {
-      this.visible = true
+      this.visible = true;
     },
     handleHide() {
-      this.visible = false
-    }
+      this.visible = false;
+    },
   },
-    mounted() {  
-    document.title = "Anil Kumar Thakur | Gallery";  
-  }, 
- 
-}
+  mounted() {
+    document.title = "Er.Anil Kumar Thakur | My Gallery";
+  },
+};
 </script>
